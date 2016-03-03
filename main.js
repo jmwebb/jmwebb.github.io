2e79c8f38de47e2227f41619d0c4f51bcf4b58ca
John Newcomb <jnewcs@stanford.edu>
2016-03-03 15:30:41 -0800

    Adding countdown

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ $( ".learn_more_container" ).click(function(e) {
          scrollTop: $('.faq').offset().top
     }, 1500, "swing"); 
 });
+
+$('#countdown').countdown('2016/03/05', function(event) {
+	console.log(event.offset);
+	$("#days").html(event.offset.days);
+	$("#hours").html(event.offset.hours);
+	$("#minutes").html(event.offset.minutes);
+	$("#seconds").html(event.offset.seconds);
+});
