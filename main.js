bcdfd6fb122c1bb3afa4750ff28dcea252af29f6
John Newcomb <jnewcs@stanford.edu>
2016-03-06 00:18:03 -0800

    Mor changes

--- main.js
+++ main.js
@@ -5,11 +5,3 @@ $( ".learn_more_container" ).click(function(e) {
          scrollTop: $('.faq').offset().top
     }, 1500, "swing"); 
 });
-
-$('#countdown').countdown('2016/03/06', function(event) {
-	console.log(event.offset);
-	$("#days").html(event.offset.days);
-	$("#hours").html(event.offset.hours);
-	$("#minutes").html(event.offset.minutes);
-	$("#seconds").html(event.offset.seconds);
-});
