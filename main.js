3e5afdd001207ac74b2ddadcbd5e886875ee68a0
John Newcomb <jnewcs@stanford.edu>
2016-05-03 13:35:59 -0700

    Making changes for Match16

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ $( ".learn_more_container" ).click(function(e) {
          scrollTop: $('.faq').offset().top
     }, 1500, "swing"); 
 });
+
+$('#countdown').countdown('2016/05/09', function(event) {
+	$("#days").html(event.offset.days);
+	$("#hours").html(event.offset.hours);
+	$("#minutes").html(event.offset.minutes);
+	$("#seconds").html(event.offset.seconds);
+});
